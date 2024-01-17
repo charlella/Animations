523ba3fa7d457430c2de6d88c873a2f894d1ebc4
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-17 13:25:05 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by charlene hoareau on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
